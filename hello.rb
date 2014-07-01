74df04632301d57f779733e34e385a16b016b50c
smonette <steph@stephmonette.com>
2014-07-01 13:42:19 -0700

    further changes to the hello message

--- hello.rb
+++ hello.rb
@@ -1,6 +1,6 @@
 puts "Hello, what's your name?"
 name = gets.chomp
-puts "What are you up to today?"
+puts "What activity are you up to today?"
 activity = gets.chomp.downcase
 
 puts "Nice to meet you, #{name}! Good luck with #{activity}."
